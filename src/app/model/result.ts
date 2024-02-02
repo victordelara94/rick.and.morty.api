94ff00fc35ae8c309bba86aca86ae247ce45e6b1
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2024-02-02 14:00:52 +0100

    Add list on app component and fix result model

--- src/app/model/result.ts
+++ src/app/model/result.ts
@@ -1,13 +1,20 @@
-import { Character } from './character';
-
 export interface Result {
-  character: Character;
+  created: string;
+  episode: string[];
+  gender: string;
+  id: number;
+  image: string;
   location: {
     name: string;
     url: string;
   };
-  image: string;
-  episode: string[];
+  name: string;
   url: string;
-  created: string;
+  status: string;
+  species: string;
+  type: string;
+  origin: {
+    name: string;
+    url: string;
+  };
 }
